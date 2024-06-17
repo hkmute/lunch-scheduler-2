cc349f3ba7dbb9913d011b30e33b0fd1f05193bd
Roy Tang <hkmute@gmail.com>
2024-06-17 14:15:29 +0800

    [APP] Try expo 51 again

--- app/src/auth/GoogleAuth.tsx
+++ app/src/auth/GoogleAuth.tsx
@@ -1,12 +1,7 @@
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
 import { Pressable, Image, StyleSheet } from "react-native";
-import {
-  ANDROID_GOOGLE_GUID,
-  EXPO_GOOGLE_GUID,
-  IOS_GOOGLE_GUID,
-  WEB_GOOGLE_GUID,
-} from "@env";
+import { ANDROID_GOOGLE_GUID, IOS_GOOGLE_GUID, WEB_GOOGLE_GUID } from "@env";
 import { ResponseType } from "expo-auth-session";
 import googleLightIconNormal from "@/assets/icons/btn_google_signin_light_normal_web.png";
 import googleLightIconPressed from "@/assets/icons/btn_google_signin_light_pressed_web.png";
@@ -20,16 +15,12 @@ const GoogleAuth: React.FC = () => {
   const { updateUser } = useUserContext();
   const login = useLogin(updateUser);
 
-  const [request, response, promptAsync] = Google.useIdTokenAuthRequest(
-    {
-      expoClientId: EXPO_GOOGLE_GUID,
-      iosClientId: IOS_GOOGLE_GUID,
-      androidClientId: ANDROID_GOOGLE_GUID,
-      webClientId: WEB_GOOGLE_GUID,
-      responseType: ResponseType.IdToken,
-    },
-    { useProxy: false }
-  );
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    iosClientId: IOS_GOOGLE_GUID,
+    androidClientId: ANDROID_GOOGLE_GUID,
+    webClientId: WEB_GOOGLE_GUID,
+    responseType: ResponseType.IdToken,
+  });
 
   useEffect(() => {
     if (response?.type === "success") {
